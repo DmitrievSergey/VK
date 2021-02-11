36f12440f4f2c79883e41302440215fd7e4dfd84
Sergey <sergei2003@bk.ru>
2021-02-11 07:54:21 +0300

    Добавил связь контролов

--- VK/LogInViewController.swift
+++ VK/LogInViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var logInTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func closeButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func logInButtonPressed(_ sender: Any) {
+    }
 }
 
