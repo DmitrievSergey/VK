09232257f1e00a1e7534ad56575e0da636db371a
Sergey <sergei2003@bk.ru>
2021-02-19 05:18:03 +0300

    add groups prototype

---
+++ VK/MyGroups/Groups.swift
@@ -0,0 +1,8 @@
+//
+//  Groups.swift
+//  VK
+//
+//  Created by Сергей Дмитриев on 19.02.2021.
+//
+
+import Foundation
---
+++ VK/MyGroups/MyGroupTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyGroupTableViewCell.swift
+//  VK
+//
+//  Created by Сергей Дмитриев on 19.02.2021.
+//
+
+import UIKit
+
+class MyGroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
