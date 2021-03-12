be255b3fed9e15302dc55a73bd3392a0021c0385
Sergey <sergei2003@bk.ru>
2021-03-12 12:27:31 +0300

    second task

--- VK/NavigationController.swift
+++ VK/NavigationController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 class NavigationController: UINavigationController, UINavigationControllerDelegate{
+    
+    fileprivate var interactivTransitionAnimator = InteractivTransitionAnimator()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let edgePanGestureRecogniser = UIPanGestureRecognizer(target: self, action: #selector(edgePanGestureStarted(_ :)))
+        //edgePanGestureRecogniser = .left
+        view.addGestureRecognizer(edgePanGestureRecogniser)
+        
         delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    @objc private func edgePanGestureStarted(_ recogniser: UIPanGestureRecognizer) {
+        print("hello there!")
+        switch recogniser.state {
+        case .began:
+            interactivTransitionAnimator.hasStarted = true
+            popViewController(animated: true)
+        case .changed:
+            let totalGestureDistance: CGFloat = 200
+            let distance = recogniser.translation(in: recogniser.view?.superview).x
+            let relativeDistance = distance / totalGestureDistance
+            let progress = max(0, min(1, relativeDistance))
+            
+            interactivTransitionAnimator.update(progress)
+            interactivTransitionAnimator.shouldFinish = progress > 0.35
+        case .ended:
+            interactivTransitionAnimator.hasStarted = false
+            
+            if interactivTransitionAnimator.shouldFinish {
+                interactivTransitionAnimator.finish()
+            } else {
+                interactivTransitionAnimator.cancel()
+            }
+        case .cancelled:
+            interactivTransitionAnimator.hasStarted = false
+            interactivTransitionAnimator.cancel()
+        default:
+            break
+        }
     }
     
 
@@ -33,6 +71,12 @@ class NavigationController: UINavigationController, UINavigationControllerDelega
             return CustomPopAnimator()
         }
         return nil
+    }
+    
+    func navigationController(_
+                                navigationController: UINavigationController,
+                              interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactivTransitionAnimator.hasStarted ? interactivTransitionAnimator : nil
     }
 
 }
